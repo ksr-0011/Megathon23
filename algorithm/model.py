7279170d4f50ee1db2585a04f078ba66ac081b86
Sannidhya Gupta <itsmrpsycho@pop-os.localdomain>
2023-10-29 04:38:33 +0530

    added model

---
+++ algorithm/model.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain import PromptTemplate
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+import chainlit as cl
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(template=custom_prompt_template, input_variables=['context', 'question'])
+    return prompt
+
+# Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type='stuff',
+        retriever=db.as_retriever(search_kwargs={'k': 2}),
+        return_source_documents=True,
+        chain_type_kwargs={'prompt': prompt}
+    )
+    return qa_chain
+
+# Loading the model
+def load_llm():
+    # Load the locally downloaded model here
+    llm = CTransformers(
+        model="llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model_type="llama",
+        max_new_tokens=1024,
+        temperature=0.0
+    )
+    return llm
+
+# QA Model Function
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+
+    return qa
+
+# Output function
+def final_result(query):
+    qa_result = qa_bot()
+    response = qa_result({'query': query})
+    return response
+
+@app.route('/qa', methods=['POST'])
+def qa_endpoint():
+    query = request.form['query']
+    ans = final_result(query)
+    print(ans['result'])
+    return jsonify({"result": ans["result"]})
+
+if __name__ == '__main__':
+    app.run()
